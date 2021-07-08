904f3b423aedc1144d532f7baf0035b8637e506d
AhmedHalabyah <ahmedhalabyah96@gmail.com>
2021-07-09 02:35:29 +0300

    solve q1

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 console.log(arrowTitle)
 /* START CODE UNDER THIS LINE */
+
+// qestion_1
+
+// make sure that the variable is in the global scope
+const favoriteFood = function () {
+
+     return myFavoriteFood;
+    
+    // TODO: Your code here
+  };
+   //let x = '10';
+   const myFavoriteFood = 'the value of `myFavoriteFood` variable';
+   // => the value of `myFavoriteFood` variable
+  
+   
